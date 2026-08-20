27447de1c0ba47a24e86f6a50a96d276195ab210
Ray Legion <ray.chen@virovek.com>
2026-08-20 14:00:56 -0700

    i think i'm done!

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
-import MobileMenu from "./mobile-menu"
 
 export default function Topbar() {
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
   return (
     <nav className="topbar">
       <div className="wrap">
-        <div>Virovek Subdomain Name</div>
+        <div className="logo-and-name">
+          <div className="virovek-logo-div">
+            <img src="images/Virovek-Logo-Web-300x103.png" className="virovek-logo" />
+          </div>
+          Subdomain Name
+        </div>
         <button className={`hamburger hamburger--slider ${hamburgerOpen ? "is-active" : ""}`} type="button" onClick={() => {
           setHamburgerOpen(!hamburgerOpen)
         }}>
@@ -15,15 +19,31 @@ export default function Topbar() {
           </span>
         </button>
 
+        {/* links when desktop */}
         <div className="links">
           <div>
-            <a href="https://store.virovek.com/" target="_blank" rel="noreferrer">Product Catalog</a>
+            <a href="https://store.virovek.com/" target="_blank" rel="noreferrer">Products</a>
           </div>
           <div>
             <a href="https://store.virovek.com/forms/custom" target="_blank" rel="noreferrer">Custom AAV Request</a>
           </div>
         </div>
-        <MobileMenu open={hamburgerOpen} />
+
+        {/* links when mobile */}
+        {
+          hamburgerOpen ?
+            <div className={`links ${hamburgerOpen ? "mobile-menu" : "mobile-menu-hidden"}`}>
+              <div className="wrap">
+                <div>
+                  <a href="https://store.virovek.com/" target="_blank" rel="noreferrer">Product Catalog</a>
+                </div>
+                <div>
+                  <a href="https://store.virovek.com/forms/custom" target="_blank" rel="noreferrer">Custom AAV Request</a>
+                </div>
+              </div>
+            </div>
+            : null
+        }
 
       </div>
     </nav>
