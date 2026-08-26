c8efbd27002b33bb94cb3bbd142c4867bd9a284c
ray-vvk <ray.chen@virovek.com>
2026-08-25 17:25:57 -0700

    adjusted eslint config to not error (only warn) when encountering the createFileRoute HMR issue

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,15 @@
-import { createRootRoute } from '@tanstack/react-router'
-import { RootLayout } from './__root-component'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
+const RootLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    <TanStackRouterDevtools />
+  </>
+)
 
 export const Route = createRootRoute({ component: RootLayout })
