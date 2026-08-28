81e7e08ab88ca6e123f41fe57986b8824e2c4e3f
ray-vvk <ray.chen@virovek.com>
2026-08-27 22:17:45 -0700

    i'm gonna try using the official github action to deploy the astro site instead of the submodule thing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   base: '/lc-dist',
-  outDir: './lc-dist'
+  outDir: './lc-dist',
+  site: 'https://ray-vvk.github.io'
 });
