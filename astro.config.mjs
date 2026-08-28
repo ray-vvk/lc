3cb9eb13cae8648169664788ba77858924bc00b5
ray-vvk <ray.chen@virovek.com>
2026-08-27 22:27:57 -0700

    changed lc-dist to lc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/lc-dist',
-  outDir: './lc-dist',
+  base: '/lc',
+  outDir: './lc',
   site: 'https://ray-vvk.github.io'
 });
