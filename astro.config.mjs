1f7f24c1c08f29c3d88edccb17b1b5b184a02fe4
ray-vvk <ray.chen@virovek.com>
2026-09-02 15:09:12 -0700

    Oh my god that was brutal. reading docs, brainstorming solutions, allat. for hours. i ended up bringing in a react component. got sorting articles by tag based on alphabetical/pubDate working.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import mdx from '@astrojs/mdx';
 import { satteri } from "@astrojs/markdown-satteri"
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   base: '/lc',
   outDir: './lc',
   site: 'https://ray-vvk.github.io',
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   markdown: { processor: satteri() }
 });
