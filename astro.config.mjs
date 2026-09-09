027bdf027a00aeafd8b485b1cdf254995c3280ee
ray-vvk <ray.chen@virovek.com>
2026-09-09 09:44:32 -0700

    pls work 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   base: '/lc',
-  site: 'https://ray-vvk.github.io/lc',
+  site: 'https://ray-vvk.github.io',
   integrations: [mdx(), react()],
   markdown: { processor: satteri() }
 });
