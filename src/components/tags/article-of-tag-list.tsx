a76795d7f2ec4832e7d01e8012cccab04a6eee19
ray-vvk <ray.chen@virovek.com>
2026-09-16 14:36:19 -0700

    moved the toggler script into its own astro component and finished making it work for tag pages too. nice. good progress today

--- src/components/tags/article-of-tag-list.tsx
+++ src/components/tags/article-of-tag-list.tsx
@@ -1,11 +1,13 @@
 import type { CollectionEntry } from "astro:content";
-import { articleSortType } from "../../store";
+import { articleSortType, articleSearchDisplaySimple } from "../../store";
 import { useStore } from "@nanostores/react";
+import ArticleSingleListing from "../article/article-single-listing";
 
 export default function ArticleOfTagList({ articlesOfTag }: {
   articlesOfTag: (CollectionEntry<"articleData"> | undefined)[]
 }) {
   const $articleSortType = useStore(articleSortType)
+  const $simple = useStore(articleSearchDisplaySimple)
   let articlesToBeShown = articlesOfTag
 
   if (articlesToBeShown.length === 0) {
@@ -59,15 +61,39 @@ export default function ArticleOfTagList({ articlesOfTag }: {
     })
   }
 
+  if ($simple) {
+    return (
+      <div>
+        {
+          articlesToBeShown
+            ? articlesToBeShown.map((article, i) => {
+              if (article) {
+                return (
+                  <div key={i}>
+                    <a href={`/lc/articles/${article?.id}`}>{article?.data.title}</a>
+                  </div>
+                )
+              }
+              return <></>
+            })
+            : "There are no articles associated with this tag."
+        }
+      </div>
+    )
+  }
+
   return (
     <div>
       {
         articlesToBeShown
-          ? articlesToBeShown.map((article, i) => (
-            <div key={i}>
-              <a href={`/lc/articles/${article?.id}`}>{article?.data.title}</a>
-            </div>
-          ))
+          ? articlesToBeShown.map((article, i) => {
+            if (article) {
+              return (
+                <ArticleSingleListing key={i} article={article} />
+              )
+            }
+            return <></>
+          })
           : "There are no articles associated with this tag."
       }
     </div>
