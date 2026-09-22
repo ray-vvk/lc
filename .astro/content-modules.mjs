e7f1fabd6f56c97fc16d71061cc27e199d96c1ef
ray-vvk <ray.chen@virovek.com>
2026-09-22 11:52:24 -0700

    delete sample articles

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,5 @@
 
 export default new Map([
-["src/data/article-data/article1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Farticle1.mdx&astroContentModuleFlag=true")],
-["src/data/article-data/article2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Farticle2.mdx&astroContentModuleFlag=true")],
-["src/data/article-data/article3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Farticle3.mdx&astroContentModuleFlag=true")],
-["src/data/article-data/article4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Farticle4.mdx&astroContentModuleFlag=true")],
 ["src/data/article-data/in-vivo-car-t-autoimmune-diseases-potential-t-cell-targeted-aav-gene-delivery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Fin-vivo-car-t-autoimmune-diseases-potential-t-cell-targeted-aav-gene-delivery.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/engineering.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fengineering.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/manufacturing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fmanufacturing.mdx&astroContentModuleFlag=true")],
