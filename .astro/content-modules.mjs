2f5073d0677f71c5cce254556618854bed428187
ray-vvk <ray.chen@virovek.com>
2026-09-22 15:04:11 -0700

    welcome article, date messing around, misc tag

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,10 @@
 
 export default new Map([
 ["src/data/article-data/in-vivo-car-t-autoimmune-diseases-potential-t-cell-targeted-aav-gene-delivery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Fin-vivo-car-t-autoimmune-diseases-potential-t-cell-targeted-aav-gene-delivery.mdx&astroContentModuleFlag=true")],
+["src/data/article-data/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Farticle-data%2Fwelcome.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/engineering.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fengineering.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/manufacturing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fmanufacturing.mdx&astroContentModuleFlag=true")],
+["src/data/tag-data/miscellaneous.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fmiscellaneous.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/protocols.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Fprotocols.mdx&astroContentModuleFlag=true")],
 ["src/data/tag-data/reviews.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Ftag-data%2Freviews.mdx&astroContentModuleFlag=true")]]);
 		
--- src/components/article/article-single-listing.tsx
+++ src/components/article/article-single-listing.tsx
@@ -4,6 +4,13 @@ import styles from "../../styles/components/article-single-listing-wrapper.modul
 export default function ArticleSingleListing({ article }: {
   article: CollectionEntry<"articleData">
 }) {
+  // because the pubDate is midnight UTC and what gets shown here is the result of showing it in our browser's time zone, we need to add some hours (10 just to be safe) to it to show the right date.
+  const pubDateAdjusted = new Date(article.data.pubDate.getTime() + 10 * 60 * 60 * 1000)
+  const f = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  })
   return (
     <>
       <a className={styles.link} href={`/lc/articles/${article.id}`}>
@@ -12,11 +19,7 @@ export default function ArticleSingleListing({ article }: {
           <div className={styles.notTitle}>
             {article.data.subtitle ? <div className={styles.subtitle}>{article.data.subtitle}</div> : null}
             <div className={styles.author}>{article.data.author}</div>
-            <div className={styles.pubDate}>Published {article.data.pubDate.toLocaleDateString("en-US", {
-              month: "long",
-              day: "numeric",
-              year: "numeric"
-            })}</div>
+            <div className={styles.pubDate}>Published {f.format(pubDateAdjusted)}</div>
             <div className={styles.tags}>Tags: {article.data.tags.map((tag, i, tags) => i === tags.length - 1 ? tag : `${tag}, `)}</div>
             <div className={styles.keywords}>
               {article.data.keywords ? "Keywords: " : null}
